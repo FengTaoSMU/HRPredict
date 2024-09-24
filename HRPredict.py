7542221c2a011b85261925eb2f2441ba4e694d6d
FengTaoSMU <104297870+FengTaoSMU@users.noreply.github.com>
2024-09-24 11:18:29 +0800

    Add files via upload

--- HRPredict.py
+++ HRPredict.py
@@ -136,7 +136,7 @@ def prot_dist_calculate(plasmid_id, fasta, ref_prot_dic, prot_vec, tmp_dir):
     return result
 
 def plasmid_embedding_generate(acc_list, ref_dir, prot_vector, fa_dir, output_dir):
-    output = open("{}input_plasmid_matrix.tsv".format(output_dir), 'w')
+    output = open("{}protein_feature_matrix.tsv".format(output_dir), 'w')
     tmp_dir = "{}tmp".format(output_dir)
     tmp_dir = dir_check(tmp_dir)
     ref_prot = "{}reference_protein.fasta".format(ref_dir)
@@ -155,33 +155,39 @@ def lineage_get(input_lineage):
     lineage = []
     with open(input_lineage) as lineage_input:
         for line in lineage_input:
-            lineage.append(line.strip())
+            lineage.append(line.strip().split(','))
     return lineage
 
-def model_lineage_get(model_dir):
-    lineage_F = lineage_get("{}{}".format(model_dir, "lineage_Family.tsv"))
-    lineage_G = lineage_get("{}{}".format(model_dir, "lineage_Genus.tsv"))
-    lineage_S = lineage_get("{}{}".format(model_dir, "lineage_Species.tsv"))
-    lineage_all = lineage_F + lineage_G
-    lineage_all = lineage_all + lineage_S
-    return lineage_all
+def host_range_output(result_dic, output):
+    for pid in result_dic:
+        output.write("{}".format(pid))
+        if result_dic[pid]:
+            hr_list = sorted(result_dic[pid])
+            for host in hr_list:
+                output.write("\t{}".format(host))
+        output.write("\n")
 
-def host_range_predict(model_dir, ref_dir, output_dir, output):
-    input_d = "{}{}".format(output_dir, "input_plasmid_matrix.tsv")
+def host_range_predict(model_dir, ref_dir, tmp_dir, output_dir):
+    input_d = "{}{}".format(tmp_dir, "protein_feature_matrix.tsv")
     rscirpt = "{}{}".format(model_dir, "HRPredict.R")
 
     cutoff_F = "{}{}".format(model_dir, "cutoff_Family.tsv")
     cutoff_G = "{}{}".format(model_dir, "cutoff_Genus.tsv")
     cutoff_S = "{}{}".format(model_dir, "cutoff_Species.tsv")
 
-    os.system("Rscript {} {} {} {} {} {} {}".format(rscirpt, model_dir, cutoff_F, cutoff_G, cutoff_S, input_d, output_dir))
+    os.system("Rscript {} {} {} {} {} {} {}".format(rscirpt, model_dir, cutoff_F, cutoff_G, cutoff_S, input_d, tmp_dir))
 
-    lineage = model_lineage_get(model_dir)
+    lineageF = lineage_get("{}{}".format(model_dir, "lineage_Family.tsv"))
+    lineageG = lineage_get("{}{}".format(model_dir, "lineage_Genus.tsv"))
+    lineageS = lineage_get("{}{}".format(model_dir, "lineage_Species.tsv"))
 
-    result_F = "{}{}".format(output_dir, "result_family.tsv")
-    result_G = "{}{}".format(output_dir, "result_genus.tsv")
-    result_S = "{}{}".format(output_dir, "result_species.tsv")
-    result_dic = {}
+    result_F = "{}{}".format(tmp_dir, "result_family.tsv")
+    result_G = "{}{}".format(tmp_dir, "result_genus.tsv")
+    result_S = "{}{}".format(tmp_dir, "result_species.tsv")
+
+    result_F_dic = {}
+    result_G_dic = {}
+    result_S_dic = {}
 
     with open(result_F) as Family_result:
         num, header = 0, []
@@ -195,14 +201,10 @@ def host_range_predict(model_dir, ref_dir, output_dir, output):
                 for item in range(len(line)):
                     if item == 0:
                         line_id = line[item]
-                        result_dic[line_id] = copy.deepcopy(lineage)
+                        result_F_dic[line_id] = []
                     else:
-                        if line[item] == "No":
-                            list_remove = []
-                            for unit in result_dic[line_id]:
-                                if header[item] in unit:
-                                    list_remove.append(unit)
-                            result_dic[line_id] = list(set(result_dic[line_id])-set(list_remove))
+                        if line[item] == "Yes":
+                            result_F_dic[line_id].append(header[item])
 
     with open(result_G) as Genus_result:
         num, header = 0, []
@@ -216,18 +218,18 @@ def host_range_predict(model_dir, ref_dir, output_dir, output):
                 for item in range(len(line)):
                     if item == 0:
                         line_id = line[item]
+                        result_G_dic[line_id] = []
                     else:
-                        if line[item] == "No":
-                            list_remove, list_new = [], []
-                            for unit in result_dic[line_id]:
-                                genus_list = unit.split(',')
-                                if len(genus_list) >= 2:
-                                    if header[item] in genus_list[1]:
-                                        f_keep = genus_list[0]
-                                        list_new.append(f_keep)
-                                        list_remove.append(unit)
-                            result_dic[line_id] = list(set(result_dic[line_id]) - set(list_remove))
-                            result_dic[line_id] = list(set(result_dic[line_id] + list_new))
+                        if line[item] == "Yes":
+                            genus_list_1, genus_list_2 = [], []
+                            genus_list_1 = [glist for glist in lineageG if header[item] in glist]
+                            genus_list_2 = [glist for glist in lineageS if header[item] in glist]
+                            if genus_list_1:
+                                if genus_list_1[0][0] in result_F_dic[line_id]:
+                                    result_G_dic[line_id].append(header[item])
+                            if genus_list_2:
+                                if genus_list_2[0][0] in result_F_dic[line_id]:
+                                    result_G_dic[line_id].append(header[item])
 
     with open(result_S) as Species_result:
         num, header = 0, []
@@ -241,47 +243,38 @@ def host_range_predict(model_dir, ref_dir, output_dir, output):
                 for item in range(len(line)):
                     if item == 0:
                         line_id = line[item]
+                        result_S_dic[line_id] = []
                     else:
-                        if line[item] == "No":
-                            list_remove, list_new = [], []
-                            for unit in result_dic[line_id]:
-                                species_list = unit.split(',')
-                                if len(species_list) >= 3:
-                                    if header[item] in species_list[2]:
-                                        g_keep = ','.join([species_list[0], species_list[1]])
-                                        list_new.append(g_keep)
-                                        list_remove.append(unit)
-                            result_dic[line_id] = list(set(result_dic[line_id]) - set(list_remove))
-                            result_dic[line_id] = list(set(result_dic[line_id] + list_new))
-    for pid in result_dic:
-        if result_dic[pid]:
-            output.write("{}".format(pid))
-            host_range = sorted(result_dic[pid])
-            for host in host_range:
-                host_list = host.split(',')
-                if len(host_list) == 3:
-                    output.write("\tf__{};g__{};s__{}".format(host_list[0], host_list[1], host_list[2]))
-                elif len(host_list) == 2:
-                    output.write("\tf__{};g__{}".format(host_list[0], host_list[1]))
-                else:
-                    output.write("\tf__{}".format(host_list[0]))
-            output.write("\n")
-        else:
-            output.write("{}\n".format(pid))
+                        if line[item] == "Yes":
+                            species_list = []
+                            species_list = [slist for slist in lineageS if header[item] in slist]
+                            if species_list:
+                                if species_list[0][1] in result_G_dic[line_id]:
+                                    result_S_dic[line_id].append(header[item])
+
+    output_F = open("{}{}".format(output_dir, "HostRange_Family.tsv"), 'w')
+    output_G = open("{}{}".format(output_dir, "HostRange_Genus.tsv"), 'w')
+    output_S = open("{}{}".format(output_dir, "HostRange_Species.tsv"), 'w')
+
+    host_range_output(result_F_dic, output_F)
+    host_range_output(result_G_dic, output_G)
+    host_range_output(result_S_dic, output_S)
+
+    output_F.close()
+    output_G.close()
+    output_S.close()
 
 def plasmid_host_range_predict(fasta, model_dir, ref_dir, output_dir):
-    output = open("{}{}".format(output_dir, "plasmid_host_range.tsv"), 'w')
-    output_dir = "{}{}/".format(output_dir, "tmp")
-    output_dir = dir_check(output_dir)
+    tmp_dir = "{}{}/".format(output_dir, "tmp")
+    tmp_dir = dir_check(tmp_dir)
     prot_vector = "{}{}".format(ref_dir, "protvec.txt")
     prot_vec = word_vector_get(prot_vector)
-    fa_dir = "{}fasta".format(output_dir)
+    fa_dir = "{}fasta".format(tmp_dir)
     fa_dir = dir_check(fa_dir)
     acc_list = accession_id_get(fasta, fa_dir)
-    plasmid_embedding_generate(acc_list, ref_dir, prot_vec, fa_dir, output_dir)
-    host_range_predict(model_dir, ref_dir, output_dir, output)
-    output.close()
-    os.system("rm -rf {}".format(output_dir))
+    plasmid_embedding_generate(acc_list, ref_dir, prot_vec, fa_dir, tmp_dir)
+    host_range_predict(model_dir, ref_dir, tmp_dir, output_dir)
+    os.system("rm -rf {}".format(tmp_dir))
 
 def main():
     input_fasta, model_dir, ref_dir, output_dir = parameter_passing()
