0018d6472d6b5d5d586c686bc806b81c68c68cf4
FengTaoSMU <104297870+FengTaoSMU@users.noreply.github.com>
2025-02-10 08:21:31 +0800

    Add files via upload

--- model/HRPredict.R
+++ model/HRPredict.R
@@ -27,43 +27,80 @@ colnames(input_word) <- c("ID", rep(paste("P", seq(1, 923), sep = "")))
 rownames(input_word) <- input_word$ID
 input_word <- input_word[, -1]
 
-## create result score and class data frame
-result_family <- data.frame(matrix(rep("No", nrow(input_word) * nrow(input_family)), nrow = nrow(input_word), ncol = nrow(input_family), byrow = TRUE))
-result_genus <- data.frame(matrix(rep("No", nrow(input_word) * nrow(input_genus)), nrow = nrow(input_word), ncol = nrow(input_genus), byrow = TRUE))
-result_species <- data.frame(matrix(rep("No", nrow(input_word) * nrow(input_species)), nrow = nrow(input_word), ncol = nrow(input_species), byrow = TRUE))
+## create result score data frame
+prob_family <- data.frame(matrix(rep(0, nrow(input_word) * nrow(input_family)), nrow = nrow(input_word), ncol = nrow(input_family), byrow = TRUE))
+prob_genus <- data.frame(matrix(rep(0, nrow(input_word) * nrow(input_genus)), nrow = nrow(input_word), ncol = nrow(input_genus), byrow = TRUE))
+prob_species <- data.frame(matrix(rep(0, nrow(input_word) * nrow(input_species)), nrow = nrow(input_word), ncol = nrow(input_species), byrow = TRUE))
 
 ## define function: return predict result
 library(e1071)
-model_result_get <- function(svm_model, input_word, cutoff){
+model_prob_get <- function(svm_model, input_word){
   pred <- predict(svm_model, input_word, decision.values = TRUE)
-  result <- data.frame(ID = rownames(input_word), distance = rep(0, nrow(input_word)), class = rep("No", nrow(input_word)))
-  result$distance <- as.numeric(attr(pred,"decision.values"))
-  result$class[result$distance > cutoff] <- "Yes"
+  result <- data.frame(ID = rownames(input_word), distance = rep(0, nrow(input_word)))
+  result$distance <- as.numeric(attr(pred, "decision.values"))
   return(result)
 }
 
 ## define function: return final predict result
-model_predict <- function(input_word, model_dir, cutoff_list, result_class){
+model_prob_predict <- function(input_word, model_dir, cutoff_list, result_class){
   for (rown in 1:nrow(cutoff_list)) {
     unit_choose <- cutoff_list[rown, 1]
     unit_choose <- gsub('[ ]', '_', unit_choose)
     unit_dir <- paste(model_dir, unit_choose, sep = "")
     unit_model <- paste(unit_dir, "_model.rds", sep = "")
     s_model <- readRDS(unit_model)
-    s_result <- model_result_get(s_model, input_word, cutoff_list[rown, 2])
-    result_class[, rown] <- s_result$class
+    s_result <- model_prob_get(s_model, input_word)
+    result_class[, rown] <- s_result$distance
     colnames(result_class)[rown] <- unit_choose
     rm(rown, unit_choose, unit_dir, s_model, s_result)
   }
-  result_class$ID <- rownames(input_word)
+  rownames(result_class) <- rownames(input_word)
+  return(result_class)
+}
+
+## get probabilities
+HR_Prob_Family <- model_prob_predict(input_word, model_f_dir, input_family, prob_family)
+HR_Prob_Genus <- model_prob_predict(input_word, model_g_dir, input_genus, prob_genus)
+HR_Prob_Species <- model_prob_predict(input_word, model_s_dir, input_species, prob_species)
+
+## output prob result
+write.table(HR_Prob_Family, file = paste(input_args[6], "prob_result_family.tsv", sep = ""), sep = ",", col.names = TRUE, row.names = TRUE, quote = FALSE)
+write.table(HR_Prob_Genus, file = paste(input_args[6], "prob_result_genus.tsv", sep = ""), sep = ",", col.names = TRUE, row.names = TRUE, quote = FALSE)
+write.table(HR_Prob_Species, file = paste(input_args[6], "prob_result_species.tsv", sep = ""), sep = ",", col.names = TRUE, row.names = TRUE, quote = FALSE)
+
+
+## create result class data frame
+result_family <- data.frame(matrix(rep("No", nrow(input_word) * nrow(input_family)), nrow = nrow(input_word), ncol = nrow(input_family), byrow = TRUE))
+result_genus <- data.frame(matrix(rep("No", nrow(input_word) * nrow(input_genus)), nrow = nrow(input_word), ncol = nrow(input_genus), byrow = TRUE))
+result_species <- data.frame(matrix(rep("No", nrow(input_word) * nrow(input_species)), nrow = nrow(input_word), ncol = nrow(input_species), byrow = TRUE))
+
+## define function: return class
+model_result_get <- function(unit_choose, input_prob, cutoff){
+  result <- data.frame(ID = rownames(input_prob), distance = rep(0, nrow(input_prob)), class = rep("No", nrow(input_prob)))
+  result$distance <- input_prob[, c(unit_choose)]
+  result$class[result$distance > cutoff] <- "Yes"
+  return(result)
+}
+
+## define function: return final predict result
+model_predict <- function(input_prob, cutoff_list, result_class){
+  for (rown in 1:nrow(cutoff_list)) {
+    unit_choose <- cutoff_list[rown, 1]
+    unit_choose <- gsub('[ ]', '_', unit_choose)
+    s_result <- model_result_get(unit_choose, input_prob, cutoff_list[rown, 2])
+    result_class[, rown] <- s_result$class
+    colnames(result_class)[rown] <- unit_choose
+    rm(rown, unit_choose, s_result)
+  }
+  result_class$ID <- rownames(input_prob)
   result_class <- result_class[, c(ncol(result_class), 1:(ncol(result_class)-1)) ]
   return(result_class)
 }
 
 ## load R package and predict
-HR_Family <- model_predict(input_word, model_f_dir, input_family, result_family)
-HR_Genus <- model_predict(input_word, model_g_dir, input_genus, result_genus)
-HR_Species <- model_predict(input_word, model_s_dir, input_species, result_species)
+HR_Family <- model_predict(HR_Prob_Family, input_family, result_family)
+HR_Genus <- model_predict(HR_Prob_Genus, input_genus, result_genus)
+HR_Species <- model_predict(HR_Prob_Species, input_species, result_species)
 
 ## output result
 write.table(HR_Family, file = paste(input_args[6], "result_family.tsv", sep = ""), sep = ",", col.names = TRUE, row.names = FALSE, quote = FALSE)
